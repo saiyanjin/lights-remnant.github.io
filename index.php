eaab9dfac3bb1e5dc43e3b44e5f34e775cc1b1fa
saiyanjin <115701346+saiyanjin@users.noreply.github.com>
2024-10-03 17:12:47 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="fr">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="images/logo.jpg">
+    <title>Light's Remnant</title>
+    <link href="css/styles.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+
+  <body>
+
+    <!-- NAVBAR -->
+     
+    <?php include 'navbar.php'; ?>
+
+    <!-- FIN NAVBAR -->
+
+    <!-- HOME  -->
+
+    <div id="home" class="vh-100 bg-black d-flex justify-content-center align-items-center text-white">
+      <h6><a href="#" class="btn-dl text-decoration-none">JOUEZ GRATUITEMENT</a></h6>
+    </div>
+
+    <!-- FIN HOME -->
+
+    <!-- JEU -->
+
+    <div id="jeu" class="bg-white vh-100">
+
+    </div>
+
+    <!-- FIN JEU -->
+
+    <!-- VALORANT -->
+
+    <div id="univers" class="bg-black vh-100">
+
+    </div>
+
+    <!-- FIN VALORANT -->
+
+    <!-- FOOTER -->
+
+    <?php include 'footer.php'; ?>
+
+    <!-- FIN FOOTER -->
+
+    <!-- SCRIPT -->
+    
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <script src="js/app.js"></script>
+
+  </body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,64 @@
+<!-- NAVBAR -->
+     
+<div class="sticky-top bg-nav position-fixed w-100">
+
+    <!-- NORMAL -->
+
+    <ul class="list-unstyled d-xl-flex justify-content-center align-items-center sticky-top bg-nav d-none">
+      <li class="mx-5 w-100">
+        <a href="#home" class="mx-3"><img src="images/logo.jpg" class="rounded" style="height: 50px; width: 50px"></a>
+        <a href="#jeu" class="fw-bolder text-decoration-none color-nav mx-3 pb-3 hover-nav-bot"><span class="p-2 rounded hover-nav">LE JEU</span></a>
+        <a href="#univers" class="fw-bolder text-decoration-none color-nav mx-3 pb-3 hover-nav-bot"><span class="p-2 rounded hover-nav">L'UNIVERS</span></a>
+        <a href="#actualites" class="fw-bolder text-decoration-none color-nav mx-3 pb-3 hover-nav-bot"><span class="p-2 rounded hover-nav">ACTUALITÉS</span></a>
+        <a href="#support" class="fw-bolder text-decoration-none color-nav mx-3 pb-3 hover-nav-bot"><span class="p-2 rounded hover-nav">SUPPORT</span></a>
+        <a href="#reseaux" class="fw-bolder text-decoration-none color-nav mx-3 pb-3 hover-nav-bot"><span class="p-2 rounded hover-nav">NOS RÉSEAUX</span></a>
+      </li>
+      <li class="mx-5">
+        <a href="#reseaux" class="fw-bolder text-decoration-none color-nav mx-3 pb-3"><span class="p-2 ps-3 pe-3 rounded btn-jouer">JOUER</span></a>
+      </li>
+    </ul>
+
+    <!-- FIN NORMAL -->
+
+    <!-- RESPONSIVE -->
+
+    <div class="d-xl-none ps-3 pt-3">
+      <ul class="list-unstyled d-flex">
+        <li class="ms-2 w-100">
+          <a href="#home" class="mx-3"><img src="images/logo.jpg" class="rounded" style="height: 50px; width: 50px"></a>
+        </li>
+        <li class="mx-5">
+          <button class="btn d-flex align-items-center btn-nav-red rounded-4 fw-bolder" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" class="bi bi-list " viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+            </svg>
+          </button>
+          
+          <div class="offcanvas offcanvas-end bg-nav-bis" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+            <div class="offcanvas-header mt-2 me-2">
+              <button type="button" class="btn-close btn-close-white btn-close-bg-transparent" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+              <p><a href="#home" class="mx-3"><img src="images/logo.jpg" class="rounded" style="height: 50px; width: 50px"></a></p>
+              <p class="my-5"><a href="#jeu" class="fw-bolder text-decoration-none color-nav mx-3 hover-nav-bot"><span class="p-2 rounded hover-nav">LE JEU</span></a></p>
+              <p class="my-5"><a href="#univers" class="fw-bolder text-decoration-none color-nav mx-3 hover-nav-bot"><span class="p-2 rounded hover-nav">L'UNIVERS</span></a></p>
+              <p class="my-5"><a href="#actualites" class="fw-bolder text-decoration-none color-nav mx-3 hover-nav-bot"><span class="p-2 rounded hover-nav">ACTUALITÉS</span></a></p>
+              <p class="my-5"><a href="#support" class="fw-bolder text-decoration-none color-nav mx-3 hover-nav-bot"><span class="p-2 rounded hover-nav">SUPPORT</span></a></p>
+              <p class="my-5"><a href="#reseaux" class="fw-bolder text-decoration-none color-nav mx-3 hover-nav-bot"><span class="p-2 rounded hover-nav">NOS RÉSEAUX</span></a></p>
+            </div>
+            <a href="#reseaux" class="text-decoration-none mx-3 mb-5">
+              <div class="offcanvas-footer btn-jouer text-center p-3 rounded-3">
+                <span class="fw-bolder color-nav mx-3">JOUER</span>
+              </div>
+            </a>
+          </div>
+        </li>
+      </ul>
+      
+    </div>
+
+    <!-- FIN RESPONSIVE -->
+
+</div>
+
+<!-- FIN NAVBAR -->
