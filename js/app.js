be971f2009a33cb8e88dae1e6801c41fb6437b30
saiyanjin <115701346+saiyanjin@users.noreply.github.com>
2024-12-17 17:40:15 +0100

    js

---
+++ js/app.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const beaconImage = document.getElementById('beaconImage');
+    let isHoldingBeacon = false;
+
+    beaconImage.addEventListener('mousedown', function (event) {
+        event.preventDefault();
+
+        isHoldingBeacon = true;
+        beaconImage.style.visibility = 'hidden';
+        document.body.style.cursor = 'url(images/beacon_on.png) 20 17, auto';
+    });
+
+    document.addEventListener('click', function (event) {
+        if (isHoldingBeacon && event.target !== beaconImage) {
+            beaconImage.style.visibility = 'visible';
+            document.body.style.cursor = 'auto';
+            isHoldingBeacon = false;
+        }
+    });
+});
