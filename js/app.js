7454ae250c64a703a9fceb22bb6537a75acb6495
saiyanjin <115701346+saiyanjin@users.noreply.github.com>
2025-01-14 12:28:29 +0100

    jouer

--- js/app.js
+++ js/app.js
@@ -1,23 +1,23 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const beaconImage = document.getElementById('beaconImage');
-    let isHoldingBeacon = false;
+// document.addEventListener("DOMContentLoaded", function() {
+//     const beaconImage = document.getElementById('beaconImage');
+//     let isHoldingBeacon = false;
 
-    beaconImage.addEventListener('mousedown', function (event) {
-        event.preventDefault();
+//     beaconImage.addEventListener('mousedown', function (event) {
+//         event.preventDefault();
 
-        isHoldingBeacon = true;
-        beaconImage.style.visibility = 'hidden';
-        document.body.style.cursor = 'url(../images/beacon_on.png) 20 17, auto';
-    });
+//         isHoldingBeacon = true;
+//         beaconImage.style.visibility = 'hidden';
+//         document.body.style.cursor = 'url(../images/beacon_on.png) 20 17, auto';
+//     });
 
-    document.addEventListener('click', function (event) {
-        if (isHoldingBeacon && event.target !== beaconImage) {
-            beaconImage.style.visibility = 'visible';
-            document.body.style.cursor = 'auto';
-            isHoldingBeacon = false;
-        }
-    });
-});
+//     document.addEventListener('click', function (event) {
+//         if (isHoldingBeacon && event.target !== beaconImage) {
+//             beaconImage.style.visibility = 'visible';
+//             document.body.style.cursor = 'auto';
+//             isHoldingBeacon = false;
+//         }
+//     });
+// });
 
 // DÉBUT BALISE
 
