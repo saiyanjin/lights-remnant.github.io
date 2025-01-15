03893d57fdacfcd1bc511007aba8b1ce740c009f
saiyanjin <115701346+saiyanjin@users.noreply.github.com>
2025-01-15 19:55:14 +0100

    maj bugs

--- js/app.js
+++ js/app.js
@@ -1,25 +1,25 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     const beaconImage = document.getElementById('beaconImage');
-//     let isHoldingBeacon = false;
-
-//     beaconImage.addEventListener('mousedown', function (event) {
-//         event.preventDefault();
-
-//         isHoldingBeacon = true;
-//         beaconImage.style.visibility = 'hidden';
-//         document.body.style.cursor = 'url(../images/beacon_on.png) 20 17, auto';
-//     });
-
-//     document.addEventListener('click', function (event) {
-//         if (isHoldingBeacon && event.target !== beaconImage) {
-//             beaconImage.style.visibility = 'visible';
-//             document.body.style.cursor = 'auto';
-//             isHoldingBeacon = false;
-//         }
-//     });
-// });
-
 // DÉBUT BALISE
+
+document.addEventListener("DOMContentLoaded", function() {
+    const beaconImage = document.getElementById('beaconImage');
+    let isHoldingBeacon = false;
+
+    beaconImage.addEventListener('mousedown', function (event) {
+        event.preventDefault();
+
+        isHoldingBeacon = true;
+        beaconImage.style.visibility = 'hidden';
+        document.body.style.cursor = 'url(../images/beacon_on.png) 20 17, auto';
+    });
+
+    document.addEventListener('click', function (event) {
+        if (isHoldingBeacon && event.target !== beaconImage) {
+            beaconImage.style.visibility = 'visible';
+            document.body.style.cursor = 'auto';
+            isHoldingBeacon = false;
+        }
+    });
+});
 
 document.addEventListener('DOMContentLoaded', function () {
     const balises = document.querySelectorAll('.balise');
